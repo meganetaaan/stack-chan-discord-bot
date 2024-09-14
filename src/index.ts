45896d989da693d8b77316bc3af6bf5a12717859
Shinya Ishikawa <ishikawa.s.1027@gmail.com>
2024-09-14 22:17:08 +0900

    fetch recent messages

--- src/index.ts
+++ src/index.ts
@@ -19,17 +19,25 @@ client.on("messageCreate", async (message: Message) => {
   if (message.author.bot) {
     return;
   }
-  console.log(message.content);
-  if (message.content.startsWith("!ping")) {
-    message.channel.send("Pong!");
-  }
+  console.debug(message.content);
   if (client.user != null && message.mentions.users.has(client.user.id)) {
     message.channel.sendTyping();
     try {
-      const response = await chatService.invoke(message.content);
+      const messages = await message.channel.messages.fetch({ limit: 10 });
+      const history = messages
+        .reverse()
+        .map((msg) => `${msg.author.displayName}: ${msg.content}`)
+        .join("\n\n");
+      console.debug(history);
+      console.debug(message.content);
+      const response = await chatService.invoke(history
+        // `${history}\n\n${message.author.displayName}: ${message.content}`
+      );
+      console.debug(response);
       message.channel.send(response);
     } catch (e) {
       if (e instanceof Error) {
+        console.log(e.message);
         message.channel.send(
           `「${e.message}」っていうエラーが出たよ。何かがうまくいっていないみたい。`
         );
